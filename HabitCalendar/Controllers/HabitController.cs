7329432a1b6b1a03d9bf44b656b6dc0661cc8cb3
meloknight <49502879+meloknight@users.noreply.github.com>
2025-01-04 18:34:55 -0800

    Working on Create functionality

--- HabitCalendar/Controllers/HabitController.cs
+++ HabitCalendar/Controllers/HabitController.cs
@@ -16,5 +16,21 @@ namespace HabitCalendar.Controllers
             List<Habit> objHabitList = _db.Habits.ToList();
             return View( objHabitList );
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create( Habit obj )
+        {
+            if ( ModelState.IsValid )
+            {
+                _db.Habits.Add( obj );
+                _db.SaveChanges();
+                return RedirectToAction( "Index", "Habit" );
+            }
+            return View();
+        }
     }
 }
--- HabitCalendar/Models/Habit.cs
+++ HabitCalendar/Models/Habit.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace HabitCalendar.Models
 {
@@ -7,10 +8,14 @@ namespace HabitCalendar.Models
         [Key]
         public int HabitId { get; set; }
         [Required]
+        [DisplayName( "Habit Name" )]
+        [MaxLength( 30 )]
+        [RegularExpression( @"^[^.\[\]\(\)]+$", ErrorMessage = "Special characters like brackets and periods are not allowed." )]
         public string HabitName { get; set; }
-
+        [DisplayName( "Habit Description" )]
         public string HabitDescription { get; set; }
         [Required]
+        [DisplayName( "Habit Display Method" )]
         public string HabitDisplayMethod { get; set; }
 
     }

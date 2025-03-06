102bec705ae8a26d1e08a3b087fb9073292b9fe6
meloknight <49502879+meloknight@users.noreply.github.com>
2025-03-06 15:41:09 -0800

    display months as words on index page. Fixed logged in username to not display anything after @ symbol.

--- HabitCalendar/Models/CalendarInfoModel.cs
+++ HabitCalendar/Models/CalendarInfoModel.cs
@@ -1,9 +1,17 @@
-﻿namespace HabitCalendar.Models
+﻿using System.Collections.ObjectModel;
+
+namespace HabitCalendar.Models
 {
     public class CalendarInfoModel
     {
         public List<CalendarDisplayModel> currentWeek { get; set; } = null!;
         public List<CalendarDisplayModel> firstWeek { get; set; } = null!;
         public List<CalendarDisplayModel> remainingWeeks { get; set; } = null!;
+        public readonly ReadOnlyCollection<string> monthNames =
+            new ReadOnlyCollection<string>( new string[]
+            {
+                "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            } );
     }
 }

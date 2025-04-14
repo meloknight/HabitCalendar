cc554b5c09a9133a82d8dde3aa4bb08db0d4ceef
meloknight <49502879+meloknight@users.noreply.github.com>
2025-04-14 16:52:53 -0700

    Fix error on CalendarDayModify.cshtml for users with no habits. Style scroll buttons and make them only visible after certain page height.

--- HabitCalendar/wwwroot/js/site.js
+++ HabitCalendar/wwwroot/js/site.js
@@ -4,6 +4,18 @@ const scrollBtnBottom = document.getElementById('scroll-to-bottom-button');
 const scrollBtnTop = document.getElementById('scroll-to-top-button');
 const calendarSection = document.getElementById('calendar-section');
 
+window.addEventListener('DOMContentLoaded', () => {
+    console.log(document.body.scrollHeight);
+
+    if (document.body.scrollHeight > 1500) {
+        scrollBtnBottom.classList.remove('scroll-button-not-visible')
+        scrollBtnTop.classList.remove('scroll-button-not-visible')
+    } else {
+        scrollBtnBottom.classList.add('scroll-button-not-visible')
+        scrollBtnTop.classList.add('scroll-button-not-visible')
+    }
+})
+
 function scrollToBottom() {
     document.documentElement.scrollTo({
         top: document.documentElement.scrollHeight,

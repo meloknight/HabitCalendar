114dd1f3982f5424444fb2fa5e308400fb65f3bb
meloknight <49502879+meloknight@users.noreply.github.com>
2025-04-30 17:06:06 -0700

    Migrated the addition of DisplayUserName to AspNetUsers

---
+++ HabitCalendar/Migrations/20250430232809_AddDisplayUserNameToApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HabitCalendar.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDisplayUserNameToApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayUserName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayUserName",
+                table: "AspNetUsers");
+        }
+    }
+}
--- HabitCalendar/Models/ApplicationUser.cs
+++ HabitCalendar/Models/ApplicationUser.cs
@@ -8,6 +8,8 @@ namespace HabitCalendar.Models
         [Required]
         public DateOnly UserStartDate { get; set; } = DateOnly.FromDateTime( DateTime.UtcNow );
 
+        public string DisplayUserName { get; set; } = string.Empty;
+
         public ICollection<Habit> Habits { get; set; } = null!;
         //public ApplicationUser()
         //{

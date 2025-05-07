5ec33391720707ea5397056b8b6a25f2e31022bb
meloknight <49502879+meloknight@users.noreply.github.com>
2025-05-07 16:32:57 -0700

    Added Info controller and View to provide a summary of the app and its usage. Began adding content and styling.

---
+++ HabitCalendar/Controllers/InfoController.cs
@@ -0,0 +1,19 @@
+﻿using HabitCalendar.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HabitCalendar.Controllers
+{
+    public class InfoController:BaseController
+    {
+        public InfoController( ApplicationDbContext db, UserManager<IdentityUser> userManager ) : base( db, userManager )
+        {
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

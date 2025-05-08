60cdd2b6d394b37238786496d1e58ef408168824
meloknight <49502879+meloknight@users.noreply.github.com>
2025-05-08 15:36:21 -0700

    Large amount of progress on email sending. Still needs debugging and styling.

--- HabitCalendar/Program.cs
+++ HabitCalendar/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>( options =>
 builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
-
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
--- HabitCalendar/Utility/EmailSender.cs
+++ HabitCalendar/Utility/EmailSender.cs
@@ -1,13 +1,44 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
 
 namespace HabitCalendar.Utility
 {
     public class EmailSender:IEmailSender
     {
-        public Task SendEmailAsync( string email, string subject, string htmlMessage )
+        private readonly IConfiguration _configuration;
+
+        public EmailSender( IConfiguration configuration )
         {
-            // Logic to send email
-            return Task.CompletedTask;
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync( string email, string subject, string htmlMessage )
+        {
+            var emailSettings = _configuration.GetSection( "EmailSettings" );
+
+            using ( var client = new SmtpClient() )
+            {
+                client.Host = emailSettings["SmtpServer"];
+                client.Port = int.Parse( emailSettings["SmtpPort"] );
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(
+                    emailSettings["SmtpUsername"],
+                    emailSettings["SmtpPassword"] );
+
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress( emailSettings["FromAddress"], emailSettings["FromName"] ),
+                    Subject = subject,
+                    Body = htmlMessage,
+                    IsBodyHtml = true
+                };
+
+                mailMessage.To.Add( email );
+
+                await client.SendMailAsync( mailMessage );
+            }
+            // return Task.CompletedTask;
         }
     }
 }
